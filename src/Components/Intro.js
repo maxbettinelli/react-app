c546523fa192f446249b627d3acb4f6412faa423
maxbettinelli <73137949+maxbettinelli@users.noreply.github.com>
2021-10-28 15:57:42 -0700

    Updated our react app and it is ready to rumble

---
+++ src/Components/Intro.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const intro = () => {
+    return (
+        <div>
+            <p>Hello World, not gonna show up</p>
+        </div>
+    );
+};
+
+export default intro;
